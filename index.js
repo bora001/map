147c30117615ecc9ab356f9a1721826595195e14
bora001 <bora.vancouver@gmail.com>
2021-12-10 12:27:12 -0800

    move mode

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ let country = {
   newyork: [40.73061, -73.935242],
   sydney: [-33.865143, 151.2099],
 };
+let zoom = 13;
 let map;
-let mode = "fly";
+let mode = "null";
 const input = document.getElementById("input");
 input.addEventListener("change", function (e) {
   let city = e.target.value;
@@ -16,11 +17,10 @@ input.addEventListener("change", function (e) {
 });
 
 //get current location
-
 const success = (pos) => {
   let crd = pos.coords;
   country.current = [crd.latitude, crd.longitude];
-  map = L.map("map").setView(country.current, 13);
+  map = L.map("map").setView(country.current, zoom);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -35,20 +35,14 @@ const error = (err) => {
 
 navigator.geolocation.getCurrentPosition(success, error);
 
-console.log(map);
-
 // const mapCnt = document.getElementById("map");
 // const cnt = document.querySelector(".cnt");
 const setLocation = (data) => {
-  console.log(data);
-  mode === "fly" ? map.flyTo(data) : map.panInside(data);
+  mode === "fly" ? map.flyTo(data, zoom) : map.setView(data, zoom);
 };
 
-const controls = document.querySelectorAll(".control_box button");
-controls.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    mode = btn.attributes.data.value;
-    btn.classList.add("active");
-    console.log(btn);
-  });
+const modeBtn = document.querySelector(".mode");
+modeBtn.addEventListener("click", function () {
+  modeBtn.classList.toggle("active");
+  modeBtn.classList.contains("active") ? (mode = "fly") : (mode = "null");
 });
